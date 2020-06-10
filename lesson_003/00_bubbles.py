bcdf58cc17d57cefde46a059d058c0429260c000
Юрий <yk2003@yandex.ru>
2020-06-10 19:22:00 +0300

    Bubble+Function

--- lesson_003/00_bubbles.py
+++ lesson_003/00_bubbles.py
@@ -4,16 +4,24 @@ import simple_draw as sd
 
 sd.resolution = (1200, 600)
 
-# Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
-point = sd.get_point(300, 300)
-#rad = 100
-#for i in range(3):, radius
-sd.circle(center_position = point)
-    #rad = rad + 5
+ #Нарисовать пузырек - три вложенных окружностей с шагом 5 пикселей
+center = sd.get_point(300, 300)
+rad = 100
+for i in range(3):
+  sd.circle(center, radius=rad)
+  rad = rad + 5
 
 
 # Написать функцию рисования пузырька, принммающую 3 (или более) параметра: точка рисования, шаг и цвет
-# TODO здесь ваш код
+def bubble(point, step, quantity, color):
+    rad = 50
+    for i in range(quantity):
+      sd.circle(point, rad, color)
+      rad = rad + step
+
+
+#point = sd.get_point(200, 200)
+#bubble(point, 5, 30, color=sd.COLOR_RED)
 
 # Нарисовать 10 пузырьков в ряд
 # TODO здесь ваш код
