5ea0b6ff4d0b413bb2240dfc254a5e75910b2b57
Юрий <yk2003@yandex.ru>
2020-06-11 00:07:07 +0300

    Upgraded zoo

--- lesson_002/05_zoo.py
+++ lesson_002/05_zoo.py
@@ -13,14 +13,16 @@ print(zoo)
 # добавьте птиц из списка birds в последние клетки зоопарка
 birds = ['rooster', 'ostrich', 'lark', ]
 #  и выведите список на консоль
-zoo = zoo + birds  # TODO: попробуйте использовать специальный метод для "расширения" списка
+#zoo = zoo + birds
+zoo.extend(birds)
 print(zoo)
 # уберите слона
 #  и выведите список на консоль
-del zoo[2]  # TODO: теперь попробуйте удалить слона по значению
+del zoo[2]
+zoo.remove('elephant')
 print(zoo)
 # выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
 # Номера при выводе должны быть понятны простому человеку, не программисту.
-# TODO здесь ваш код
+
 print('Cage of lion is', (zoo.index('lion')+1))
 print('Cage of lark is', (zoo.index('lark')+1))
