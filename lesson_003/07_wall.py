96cee63524c01e09dcf70b01381c612c657d23e9
Юрий <yk2003@yandex.ru>
2020-06-12 20:06:58 +0300

    Bricks

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import simple_draw
+import simple_draw as sd
 
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+for x in range(0, 601, 100):
+    for y in range(0, 600, 50):
+       if y%100 == 0:
+        sd.rectangle(sd.get_point(x-50 , y), sd.get_point(x + 50, y + 50), color=sd.COLOR_YELLOW, width=1)
+       else:
+           sd.rectangle(sd.get_point(x, y), sd.get_point(x + 100, y + 50), color=sd.COLOR_YELLOW, width=1)
+
+
+
+
+
 # Подсказки:
 #  Для отрисовки кирпича использовать функцию rectangle
 #  Алгоритм должен получиться приблизительно такой:
@@ -17,4 +27,4 @@ import simple_draw
 #           вычисляем правый нижний и левый верхний углы кирпича
 #           рисуем кирпич
 
-simple_draw.pause()
+sd.pause()
