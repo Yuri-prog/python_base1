2097dfab3a62166f3bf36f0766db9801241cbe5a
Юрий <yk2003@yandex.ru>
2020-06-14 12:54:10 +0300

    Brick into a hole

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -42,24 +42,43 @@ push_paper(11, 9)
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
 # hole_x, hole_y = 8, 9
-# brick_x, brick_y, brick_z = 11, 10, 2
-# brick_x, brick_y, brick_z = 11, 2, 10
-# brick_x, brick_y, brick_z = 10, 11, 2
-# brick_x, brick_y, brick_z = 10, 2, 11
-# brick_x, brick_y, brick_z = 2, 10, 11
-# brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
-# brick_x, brick_y, brick_z = 3, 6, 5
-# brick_x, brick_y, brick_z = 6, 3, 5
-# brick_x, brick_y, brick_z = 6, 5, 3
-# brick_x, brick_y, brick_z = 5, 6, 3
-# brick_x, brick_y, brick_z = 5, 3, 6
-# brick_x, brick_y, brick_z = 11, 3, 6
-# brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
-# brick_x, brick_y, brick_z = 6, 3, 11
-# brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
-# (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+x = brick_x, brick_y, brick_z = 11, 10, 2
+
+
+list_1 = brick_x, brick_y, brick_z = 11, 2, 10
+list_2 = brick_x, brick_y, brick_z = 10, 11, 2
+list_3 = brick_x, brick_y, brick_z = 10, 2, 11
+list_4 = brick_x, brick_y, brick_z = 2, 10, 11
+list_5 = brick_x, brick_y, brick_z = 2, 11, 10
+list_6 = brick_x, brick_y, brick_z = 3, 5, 6
+list_7 = brick_x, brick_y, brick_z = 3, 6, 5
+list_8 = brick_x, brick_y, brick_z = 6, 3, 5
+list_9 = brick_x, brick_y, brick_z = 6, 5, 3
+list_10 = brick_x, brick_y, brick_z = 5, 6, 3
+list_11 = brick_x, brick_y, brick_z = 5, 3, 6
+list_12 = brick_x, brick_y, brick_z = 11, 3, 6
+list_13 = brick_x, brick_y, brick_z = 11, 6, 3
+list_14 = brick_x, brick_y, brick_z = 6, 11, 3
+list_15 = brick_x, brick_y, brick_z = 6, 3, 11
+list_16 = brick_x, brick_y, brick_z = 3, 6, 11
+list_17 = brick_x, brick_y, brick_z = 3, 11, 6
+# (просто раскоментировать нужную строку и проверить свой код)
+big_list = [list_1, list_2, list_3, list_4, list_5, list_6, list_7, list_8, list_8, list_10, list_11, list_12, list_13, list_14, list_15, list_16, list_17, ]
+
+def push_brick(brick_x, brick_y, brick_z):
+    list_hole = hole_x, hole_y = 8, 9
+    new_list = []
+    for x in big_list:
+        x = sorted(x)  #если хотя бы 2 стороны крпича проходят в отверстие, пройдет и весь кирпич. Поэтому сторону
+        del x[2]               # с максимальной длиной отбрасываем.
+        new_list.append(x)
+    for k, i in enumerate(new_list, 1):
+    #for i in new_list:
+        if (list_hole[0] >= i[0] and list_hole[1] >= i[1]) or (list_hole[0] >= i[1] and list_hole[1] >= i[0]):
+            print('Кирпич №', k, 'проходит в отверстие',)
+        else:
+            print('Кирпич №', k, 'не проходит в отверстие',)
+
+
+push_brick(brick_x, brick_y, brick_z)
