e780412479280b313614805a97877f96a48bfdd1
Юрий <yk2003@yandex.ru>
2020-06-26 17:08:57 +0300

    Inhabitants of the district

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -4,8 +4,12 @@
 # Формат вывода: На районе живут ...
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
+from shlex import join
 
-# TODO здесь ваш код
+import room_1
+import room_2
+district = room_1.folks + room_2.folks
+print('На районе живут', ', '.join(district),'.')
 
 
 
