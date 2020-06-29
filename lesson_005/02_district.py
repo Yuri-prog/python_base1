2179db744040200e69f8e1bf2c79572f8a0c1623
Юрий <yk2003@yandex.ru>
2020-06-29 10:15:24 +0300

    District inhabitants

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -6,12 +6,21 @@
 # https://docs.python.org/3/library/stdtypes.html#str.join
 from shlex import join
 
-# TODO: в этом задании нужно работать с модулем district
 
-import room_1
-import room_2
-district = room_1.folks + room_2.folks
-print('На районе живут', ', '.join(district),'.')
+
+import district.central_street.house1.room1 as ch1r1
+import district.central_street.house1.room2 as ch1r2
+import district.central_street.house2.room1 as ch2r1
+import district.central_street.house2.room2 as ch2r2
+import district.soviet_street.house1.room1 as sh1r1
+import district.soviet_street.house1.room2 as sh1r2
+import district.soviet_street.house2.room1 as sh2r1
+import district.soviet_street.house2.room2 as sh2r2
+
+
+
+district = ch1r1.folks + ch1r2.folks + ch2r1.folks + ch2r2.folks + sh1r1.folks + sh1r2.folks + sh2r1.folks + sh2r2.folks
+print('На районе живут', ', '.join(district), '.')
 
 
 
