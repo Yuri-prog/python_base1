4367f209c35d9b8f717f76cf26e16f116ee82675
Юрий <yk2003@yandex.ru>
2020-08-11 21:19:20 +0300

    War And Peace Letter Counting

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -25,7 +25,41 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+file_name = 'voyna-i-mir.txt'
+
+
+class Text:
+    alphabet = list(range(0x0041, 0x005B)) + list(range(0x0061, 0x007B)) + list(range(0x0410, 0x0450))
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.total_quant = []
+        self.letter_quant = ()
+        self.sum = 0
+
+    def count_letters(self):
+        with open(file_name, 'r', encoding='cp1251') as file:
+            total = file.read()
+            for letter in self.alphabet:
+                self.letter_quant = (chr(letter), total.count(chr(letter)))
+                self.total_quant.append(self.letter_quant)
+                self.total_quant.sort(key=lambda i: i[1], reverse=True)
+
+    def print_table(self):
+        print('+{txt:-<6}+{txt:-<10}+'.format(txt=''))
+        print('|{txt1:^6}|{txt2:^10}|'.format(txt1='Буква', txt2='Частота'))
+        print('+{txt:-<6}+{txt:-<10}+'.format(txt=''))
+        for self.letter_quant in self.total_quant:
+            print('|{txt1:^6}|{txt2:^10}|'.format(txt1=self.letter_quant[0], txt2=self.letter_quant[1]))
+            self.sum += self.letter_quant[1]
+        print('+{txt:-<6}+{txt:-<10}+'.format(txt=''))
+        print('|{txt1:^6}|{txt2:^10}|'.format(txt1='Итого', txt2=self.sum))
+        print('+{txt:-<6}+{txt:-<10}+'.format(txt=''))
+
+
+file = Text('voyna-i-mir.txt')
+file.count_letters()
+file.print_table()
 
 # После зачета первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 # Имеется файл events.txt вида:
 #
 # [2018-05-17 01:55:52.665804] NOK
@@ -23,7 +24,41 @@
 #   см https://refactoring.guru/ru/design-patterns/template-method
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
-# TODO здесь ваш код
+file_name = 'events.txt'
+
+
+class Parser:
+
+    def __init__(self, file_name, new_file_name):
+        self.file_name = file_name
+        self.new_file_name = new_file_name
+        self.nok_quant = []
+        self.pair_list = []
+        self.new_pair_list = []
+
+    def count_events(self):
+        with open(file_name, 'r', encoding='cp1251') as file:
+            for line in file:
+                if line.count('NOK'):
+                    line = line[0:17] + ']'
+                    self.nok_quant.append(line)
+            for line in self.nok_quant:
+                pair = line + ' ' + str(self.nok_quant.count(line))
+                self.pair_list.append(pair)
+            for pair in self.pair_list:
+                if pair not in self.new_pair_list:
+                    self.new_pair_list.append(pair)
+            return str('\n'.join(self.new_pair_list))
+
+    def file_write(self):
+        new_file = open(self.new_file_name, mode='w', encoding='utf8')
+        new_file.write(self.count_events())
+        new_file.close()
+
+
+file = Parser('events.txt', 'new_events.txt')
+file.count_events()
+file.file_write()
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам
