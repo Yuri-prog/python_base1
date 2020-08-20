ba9a0f484463c0bc100b23717f9dd3709cf88c21
Юрий <yk2003@yandex.ru>
2020-08-20 12:08:15 +0300

    Вопрос

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -26,38 +26,57 @@ from collections import Counter
 #   и https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
 
 file_name = 'events.txt'
+new_file_name = 'new_events.txt'
 
 
 class Parser:
     new_pair_dict = {}
+    time = ''
 
     def __init__(self, file_name, new_file_name):
         self.file_name = file_name
         self.new_file_name = new_file_name
         self.new_pair_list = []
 
-    def count_events(self):
+    def count_events(self, time):
         with open(file_name, 'r', encoding='cp1251') as file:
-            for i, line in enumerate(file):
-                if line.count('NOK'):
-                    line = line[0:17] + ']'
-                    if line not in self.new_pair_dict:
-                        self.new_pair_dict[line] = 1
+            for line in file:
+                if 'NOK' in line:
+                    def count_time():
+                        if line not in self.new_pair_dict:
+                            self.new_pair_dict[line] = 1
+                        else:
+                            self.new_pair_dict[line] += 1
+
+                    if time == 'min':
+                        line = line[0:17] + ']'
+                        count_time()
+                    elif time == 'hour':
+                        line = line[0:14] + ']'
+                        count_time()
+                    elif time == 'month':
+                        line = line[0:8] + ']'
+                        count_time()
                     else:
-                        self.new_pair_dict[line] += 1
+                        print('Ошибка ввода')
+                        break
+                else:
+                    continue
             for key, value in self.new_pair_dict.items():
                 self.new_pair_list.append(key + ' ' + str(value))
             return str('\n'.join(self.new_pair_list))
 
     def file_write(self):
         new_file = open(self.new_file_name, mode='w', encoding='utf8')
-        new_file.write(self.count_events())
+        new_file.write(self.count_events(self.time))
         new_file.close()
 
 
 file = Parser('events.txt', 'new_events.txt')
-file.count_events()
+file.count_events('hour')
 file.file_write()
+
+#TODO: Вопрос. Почему пишет Ошибку ввода и делает двойную запись в файл?
 
 # payload = {
 #     '1 a': 'NOK',
@@ -96,3 +115,4 @@ file.file_write()
 # # После зачета первого этапа нужно сделать группировку событий
 # #  - по часам
 # #  - по месяцу
+
