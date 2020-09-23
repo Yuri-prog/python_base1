b2c97fc8e9e6bcf493a7f526962f5a84d5865cf4
Юрий <yk2003@yandex.ru>
2020-09-23 09:45:34 +0300

    With threads №3

--- lesson_012/02_volatility_with_threads.py
+++ lesson_012/02_volatility_with_threads.py
@@ -23,42 +23,38 @@ from threading import Thread
 
 
 class Volatility(Thread):
-    directory = 'trades'
-    files = os.listdir(directory)
+    volatility_list = []
+    null_volatility = []
 
-    def __init__(self, name, *args, **kwargs):
+    def __init__(self, file_name, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.name = name
+        self.file_name = file_name
         self.ticker_name = ''
         self.volatility = 0
-        self.file_name = ''
-        self.null_volatility = []
-        self.volatility_list = []
+        self.ticker_prices = []
 
     def run(self):
-
-        for self.file_name in self.files:
-            self.ticker_prices = []
-            self.file_name = f'trades\\\{self.file_name}'
-            with open(self.file_name, 'r', encoding='utf8') as file:
-                for line in file:
-                    line = (line.split(','))
-                    self.ticker_name = line[0]
-                    price = line[2]
-                    if price[0].isnumeric():
-                        price = float(price)
-                        self.ticker_prices.append(price)
+        self.file_name = f'trades\\\{self.file_name}'
+        with open(self.file_name, 'r', encoding='utf8') as file:
+            for line in file:
+                line = (line.split(','))
+                self.ticker_name = line[0]
+                price = line[2]
+                if price[0].isnumeric():
+                    price = float(price)
+                    self.ticker_prices.append(price)
             half_sum = (max(self.ticker_prices) + min(self.ticker_prices)) / 2
             self.volatility = (max(self.ticker_prices) - min(self.ticker_prices)) / half_sum * 100
             if self.volatility == 0.0:
                 self.null_volatility.append(self.ticker_name)
             else:
                 self.volatility_list.append((self.ticker_name, self.volatility))
-
         return self.volatility_list, self.null_volatility
 
 
 class Tickers():
+    directory = 'trades'
+    files = os.listdir(directory)
 
     def __init__(self):
         self.min_volatility = {}
@@ -67,10 +63,10 @@ class Tickers():
         self.volatility_list = []
 
     def main(self):
-        for i in range(5):
-            name = f'Thread #{(i + 1)}'
-            my_thread = Volatility(name)
+        my_threads = [Volatility(file_name=file_name) for file_name in self.files]
+        for my_thread in my_threads:
             my_thread.start()
+        for my_thread in my_threads:
             my_thread.join()
             my_thread.volatility_list.sort(key=lambda i: i[1])
             self.min_volatility = dict(my_thread.volatility_list[2::-1])
@@ -87,16 +83,6 @@ class Tickers():
 
 start_time = datetime.now()
 all_threads = Tickers()
-if __name__ == '__main__':
-    all_threads.main()
-
-# TODO: Но проблема в том, что у вас программа делает два раза одно и то же. Так мы пользы из задания не извлечем.
-# TODO: Cделайте так, чтобы каждый файл обрабатывался в отдельном треде, а класс Tickers пусть управляет тредами и собирает с них данные.
-# TODO: По-моему, у меня опять потоки повторяют одно и то же, но как привязать конкретный поток к конкретному файлу я понять пока не могу.
-
-# TODO: Можно сделать двумя способами. Первый, это заставить Volatility работать только с одним файлом.
-# TODO: То есть получить где-нибудь снаружи список файлов, затем создать для каждого полученного имени файла класс Volatility, передав в инит имя файла, и внутри него этот файл обработать.
-# TODO: Второй способ - это создать фиксированное число классов Volatility, передав в них куски списка файлов. Каждый тред обработает свою пачку файлов, а Tickers соберет результаты.
-
+all_threads.main()
 end_time = datetime.now()
 print("Время выполнения: ", end_time - start_time)
