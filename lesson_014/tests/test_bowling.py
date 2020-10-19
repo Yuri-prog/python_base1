70174658e036161701c40b558099fa3e7868caac
Юрий <yk2003@yandex.ru>
2020-10-19 23:39:35 +0300

    Bowling upgrade #3

--- lesson_014/tests/test_bowling.py
+++ lesson_014/tests/test_bowling.py
@@ -1,5 +1,5 @@
 import unittest
-from lesson_014.bowling import get_score
+from bowling import get_score
 # Сейчас при попытке запуска теста из под командной строки не находит lesson_014
 # TODO Пути старайтесь указывать относительно рабочей директории (той, в которой лежит главный запускаемый файл)
 # TODO т.к. здесь у нас проект состоит из нескольких "мини"-проектов, то можно выполнить хитрый приём, явно указав
@@ -7,25 +7,28 @@ from lesson_014.bowling import get_score
 # TODO Сделать это можно либо в Run - Edit configurations
 # TODO Либо можно просто выделить нужную папку как source root
 # TODO для этого надо нажать на неё правой кнопкой - mark directory as - source root
-
+# TODO Пробовал ставить в командной строке основным каталогом и lesson_014 и tests, указывал на рабочую директорию, ничего не помогает.
+#      Заработало только когда скопировал файл bowling в папку tests.
 
 class MySortTest(unittest.TestCase):
 
     def test_normal(self):
-        result = get_score('557/23XX-3X82453-')
-        self.assertEqual(result, 115)
+        result = get_score('547/23XX-3X81453-')
+        self.assertEqual(result, 113)
         with self.assertRaises(ValueError):
-            get_score('557/23XX-3X82453')
+            get_score('547/23XX-3X81453')
         with self.assertRaises(ValueError):
-            get_score('557/23XX-3X82453-5')
+            get_score('547/23XX-3X81453-5')
 
     def test_wrong_symbol(self):
         with self.assertRaises(ValueError):
-            get_score('557/23XX-3y82453-')
+            get_score('547/23XX-3y81453-')
+        with self.assertRaises(ValueError):
+            get_score('547/23XX-3x8140-')
 
     def test_lower_case(self):
-        result = get_score('557/23Xx-3x82453-')
-        self.assertEqual(result, 115)
+        result = get_score('547/23Xx-3x81453-')
+        self.assertEqual(result, 113)
 
     def test_all_strikes(self):
         result = get_score('XXXXXXXXXX')
@@ -42,8 +45,8 @@ class MySortTest(unittest.TestCase):
             get_score('1/2/3/4/5/6/7/8/9///')
 
     def test_all_numbers(self):
-        result = get_score('12345564738291341912')
-        self.assertEqual(result, 80)
+        result = get_score('12345463728121341812')
+        self.assertEqual(result, 68)
         with self.assertRaises(ValueError):
             get_score('12345564738291343912')
 
